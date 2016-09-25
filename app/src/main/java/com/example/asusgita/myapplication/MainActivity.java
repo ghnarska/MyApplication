2e85d38ae41ff283f2b8f89df544d57d4599b501
ghnarska <ghina.rosika@gmail.com>
2016-09-25 22:46:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/asusgita/myapplication/MainActivity.java
@@ -0,0 +1,201 @@
+package com.example.asusgita.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button0, button1, button2, button3, button4, button5, button6,
+            button7, button8, button9, buttonAdd, buttonSub, buttonDivision,
+            buttonMul, buttonC, buttonEqual;
+
+    EditText editText2;
+
+    float mValueOne, mValueTwo;
+
+    boolean mAddition, mSubtract, mMultiplication, mDivision;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button0 = (Button) findViewById(R.id.button0);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+        button6 = (Button) findViewById(R.id.button6);
+        button7 = (Button) findViewById(R.id.button7);
+        button8 = (Button) findViewById(R.id.button8);
+        button9 = (Button) findViewById(R.id.button9);
+        buttonDot = (Button) findViewById(R.id.buttonDot);
+        buttonAdd = (Button) findViewById(R.id.buttonAdd);
+        buttonSub = (Button) findViewById(R.id.buttonSub);
+        buttonMul = (Button) findViewById(R.id.buttonMul);
+        buttonDivision = (Button) findViewById(R.id.buttonDivision);
+        buttonC = (Button) findViewById(R.id.buttonC);
+        buttonEqual = (Button) findViewById(R.id.buttonEqual);
+        editText2 = (EditText) findViewById(R.id.editText2);
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText2.setText(editText2.getText() + "1");
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText2.setText(editText2.getText() + "2");
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText2.setText(editText2.getText() + "3");
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText2.setText(editText2.getText() + "4");
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText2.setText(editText2.getText() + "5");
+            }
+        });
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText2.setText(editText2.getText() + "6");
+            }
+        });
+
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText2.setText(editText2.getText() + "7");
+            }
+        });
+
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText2.setText(editText2.getText() + "8");
+            }
+        });
+
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText2.setText(editText2.getText() + "9");
+            }
+        });
+
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText2.setText(editText2.getText() + "0");
+            }
+        });
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (editText2 == null) {
+                    editText2.setText("");
+                } else {
+                    mValueOne = Float.parseFloat(editText2.getText() + "");
+                    mAddition = true;
+                    editText2.setText(null);
+                }
+            }
+        });
+
+        buttonSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(editText2.getText() + "");
+                mSubtract = true;
+                editText2.setText(null);
+            }
+        });
+
+        buttonMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(editText2.getText() + "");
+                mMultiplication = true;
+                editText2.setText(null);
+            }
+        });
+
+        buttonDivision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(editText2.getText() + "");
+                mDivision = true;
+                editText2.setText(null);
+            }
+        });
+
+        buttonEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+                mValueTwo = Float.parseFloat(editText2.getText() + "");
+
+                if (mAddition == true) {
+
+                    editText2.setText(mValueOne + mValueTwo + "");
+                    mAddition = false;
+                }
+
+
+                if (mSubtract == true) {
+                    editText2.setText(mValueOne - mValueTwo + "");
+                    mSubtract = false;
+                }
+
+                if (mMultiplication == true) {
+                    editText2.setText(mValueOne * mValueTwo + "");
+                    mMultiplication = false;
+                }
+
+                if (mDivision == true) {
+                    editText2.setText(mValueOne / mValueTwo + "");
+                    mDivision = false;
+                }
+            }
+        });
+
+        buttonC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText2.setText("");
+            }
+        });
+
+        buttonDot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText2.setText(editText2.getText() + ".");
+            }
+        });
+    }
+}
